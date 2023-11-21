9b048be7ba4495c49e83211d4c11682388962071
Sam Chung <tieoneease@gmail.com>
2023-11-21 04:10:45 -0800

    init rockon

--- keyboards/fingerpunch/rockon_bp/keymaps/default/keymap.c
+++ keyboards/fingerpunch/rockon_bp/keymaps/default/keymap.c
@@ -9,7 +9,6 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _QWERTY,
-    _COLEMAK,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -30,35 +29,26 @@ bool is_caps_lock_on;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_rockon_bp(
-    KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_LPRN,                            KC_RPRN,   KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_BSPC,
-    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_LBRC,                            KC_RBRC,   KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,
-    KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_LCBR,                            KC_RCBR,   KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
+    KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_LPRN,                            KC_RPRN,   KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_GRV,
+    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_BTN2,                            KC_PGUP,   KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSPC,
+    KC_LCTL,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_BTN1,                            KC_PGDN,   KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
     KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      ADJUST,                             ADJUST,    KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_ENT,
-                          KC_LCTL,   KC_LGUI,   LOWER,     KC_SPC,    KC_LALT,   KC_MUTE, LCTL(KC_BSPC),  KC_RALT,   KC_SPC,    RAISE,     KC_RGUI,   KC_RCTL
-),
-
-
-[_COLEMAK] = LAYOUT_rockon_bp(
-    KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_LPRN,                            KC_RPRN,   KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_BSPC,
-    KC_TAB,    KC_Q,      KC_W,      KC_F,      KC_P,      KC_B,      KC_LBRC,                            KC_RBRC,   KC_J,      KC_L,      KC_U,      KC_Y,      KC_SCLN,   KC_BSLS,
-    KC_CAPS,   KC_A,      KC_R,      KC_S,      KC_T,      KC_G,      KC_LCBR,                            KC_RCBR,   KC_M,      KC_N,      KC_E,      KC_I,      KC_O,      KC_QUOT,
-    KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_D,      KC_V,      ADJUST,                             ADJUST,    KC_K,      KC_H,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_ENT,
-                          KC_LCTL,   KC_LGUI,   LOWER,     KC_SPC,    KC_LALT,   KC_MUTE, LCTL(KC_BSPC),  KC_RALT,   KC_SPC,    RAISE,     KC_RGUI,   KC_RCTL
+                          KC_DOWN,   KC_UP,   KC_LALT,   KC_LGUI,   KC_LALT,     LOWER,   KC_SPC,       RAISE,     KC_SPC,    KC_DOWN,     KC_LEFT,   KC_RIGHT
 ),
 
 
 [_LOWER] = LAYOUT_rockon_bp(
-    KC_GRV,        _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       KC_MINS,       KC_EQL,        _______,
     _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
-    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
-    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
-                                  _______,       _______,       _______,       _______,       _______,    _______,       _______,   _______,       _______,       _______,       _______,       _______
+    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       KC_MINS,       KC_EQL,        _______,       _______,       _______,
+    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       KC_LBRC,       KC_RBRC,       KC_BSLS,        KC_GRV,       _______,
+    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       KC_LPRN,       KC_RPRN,       _______,       _______,       _______,
+                                  _______,       _______,       _______,       _______,       _______,    _______,       _______,   _______,       _______,       _______,       KC_DOWN,       KC_UP
 ),
 
 [_RAISE] = LAYOUT_rockon_bp(
-    KC_GRV,        _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       KC_MINS,       KC_EQL,        _______,
-    _______,       KC_PGUP,       KC_UP,         KC_PGDN,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
-    KC_HOME,       KC_LEFT,       KC_DOWN,       KC_RGHT,       KC_END,        _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
+    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,        _______,
+    _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
+    _______,       KC_VOLD,       KC_VOLU,       _______,       _______,       _______,       _______,                              _______,       _______,       RCS(KC_TAB),  LCTL(KC_TAB),   _______,       _______,       _______,
     _______,       _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
                                   _______,       _______,       _______,       _______,       _______,    _______,       _______,   _______,       _______,       _______,       _______,       _______
 ),
@@ -66,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] =  LAYOUT_rockon_bp(
     QK_BOOT,         KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,         _______,                              _______,       KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,        _______,
     _______,       RGB_TOG,       RGB_RMOD,      RGB_MOD,       _______,       TG(_QWERTY),   _______,                              _______,       _______,       _______,       _______,       KC_F11,        KC_F12,        _______,
-    _______,       RGB_SPI,       RGB_HUI,       RGB_SAI,       RGB_VAI,       TG(_COLEMAK),  _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
+    _______,       RGB_SPI,       RGB_HUI,       RGB_SAI,       RGB_VAI,       _______,  _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
     _______,       RGB_SPD,       RGB_HUD,       RGB_SAD,       RGB_VAD,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
                                   _______,       _______,       _______,       _______,       _______,    _______,       _______,   _______,       _______,       _______,       _______,       _______
 )
